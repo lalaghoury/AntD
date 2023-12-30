75c68be9e145251cc265deb160d3a50924cea709
lalaghoury <127485169+lalaghoury@users.noreply.github.com>
2023-12-30 08:54:59 +0000

    aa

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/ContactApp/CoctactForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoctactForm() {
+  return (
+    <div>CoctactForm</div>
+  )
+}
+
+export default CoctactForm
